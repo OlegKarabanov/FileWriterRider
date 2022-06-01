2e4d4ca6b3714c480b594e0567da3f7911ac9127
OlegKarabanov <salem27@mail.ru>
2022-06-01 19:33:03 +0300

    Implemented saveText in MainActivity

--- app/src/main/java/ru/synergi/filewriterrider/MainActivity.java
+++ app/src/main/java/ru/synergi/filewriterrider/MainActivity.java
@@ -4,25 +4,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
-private final static String FILE_NAME = "content.txt";
+    private final static String FILE_NAME = "content.txt";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    //сохранение файла работает в рамках онклика
+
     public void saveText(View view) {
         FileOutputStream fos = null;
+
+        try {
+            EditText textBox = (EditText) findViewById(R.id.editor);
+            String text = textBox.getText().toString();
+
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Файл успешно сохранен", Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
-    public void openText(View view) {
-        FileInputStream fin = null;
-        TextView textView = (TextView) findViewById(R.id.text);
-    }
+
+
+
 }
+
+
+
+
+
+
